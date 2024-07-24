9459ea58a4396de3dc8d9c19e92b1fa3d2ca4748
OkaniYoshiii <dragut31620@gmail.com>
2024-07-24 23:07:21 +0200

    Création class Autoloader

--- public/index.php
+++ public/index.php
@@ -2,8 +2,15 @@
 
 declare(strict_types=1);
 
+use App\Autoloader;
+
 require_once '../vendor/autoload.php';
+require_once '../src/Autoloader.php';
 
 $loader = new \Twig\Loader\FilesystemLoader('../templates');
 $twig = new \Twig\Environment($loader, ['strict_variables' => true]);
+
+$autoloader = new Autoloader();
+$autoloader->register();
+
 echo $twig->render('index.html.twig');
---
+++ src/Autoloader.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Exception;
+
+class Autoloader
+{
+    public function register()
+    {
+        spl_autoload_register(function(string $class) {
+            if(!str_starts_with($class, 'App')) throw new Exception('Can\'t load class ' . $class . ' : namespace doesn\'t start with \'App\'');
+            $classStr = substr_replace($class, 'src', 0, strlen('app'));
+            
+            $pos = strrpos($classStr, '\\');
+            $className = substr($classStr, $pos);
+
+            $classStr = strtolower($classStr);
+            $classStr = substr_replace($classStr, $className, $pos);
+
+            $filepath = '..\\';
+            $filepath .= str_replace('\\', DIRECTORY_SEPARATOR, $classStr);
+            $filepath .= '.php';
+
+            if(file_exists($filepath)) throw new Exception('Class ' . $class . ' not found in ' . $filepath . '. The filename doesn\'t match the name of the class or the namespace doesn\'t match the folder structure.');
+            require_once $filepath;
+        });
+    }
+}
