d61e225984d11006da2358911c50e27f3b102305
OkaniYoshiii <dragut31620@gmail.com>
2024-07-24 21:29:45 +0200

    Création classe Request

---
+++ src/Request.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App;
+
+class Request
+{
+    private string $method;
+    private string $path;
+    private string $queryString;
+    private array $parameters;
+
+    public function __construct()
+    {
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $this->queryString = $_SERVER['QUERY_STRING'];
+        $this->parameters = ($this->method === 'GET') ? $_GET : null;
+    }
+
+    /**
+     * Get the value of method
+     */ 
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * Get the value of path
+     */ 
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * Get the value of queryString
+     */ 
+    public function getQueryString()
+    {
+        return $this->queryString;
+    }
+
+    /**
+     * Get the value of parameters
+     */ 
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+}
