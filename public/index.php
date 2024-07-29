4c5756d839f0052d45d6f4b0bb4f17d13327b9a1
OkaniYoshiii <dragut31620@gmail.com>
2024-07-29 22:37:26 +0200

    Autoload via composer

--- public/index.php
+++ public/index.php
@@ -2,13 +2,9 @@
 
 declare(strict_types=1);
 
-use App\Autoloader;
 use App\Router;
 
 require_once '../vendor/autoload.php';
-require_once '../src/Autoloader.php';
-
-$autoloader = Autoloader::getInstance();
 
 $loader = new \Twig\Loader\FilesystemLoader('../templates');
 $twig = new \Twig\Environment($loader, ['strict_variables' => true]);
